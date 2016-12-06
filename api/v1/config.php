20b9e52e08b11075c45fb37b6776a5bbab9ad480
Michael Kilday <mike@dragonfrugal.com>
2016-12-06 15:35:31 -0500

    Fixed test data left in config
    Fixed test data left in config

--- api/v1/config.php
+++ api/v1/config.php
@@ -2,7 +2,7 @@
 
 $version='0.1.1'; // 2016/Dec/6th
 
-$create_api_key_password = 'z';  // YOU MUST CREATE A PASSWORD HERE TO USE THE API KEY CREATION PAGE create-api-key.php
+$create_api_key_password = '';  // YOU MUST CREATE A PASSWORD HERE TO USE THE API KEY CREATION PAGE create-api-key.php
 
 // API Modules you want activated, and corrisponding endpoint ip_address:port_number
 $allowed_apis = array(
